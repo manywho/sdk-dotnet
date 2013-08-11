d1985a91fa7c534f79ac8192437bd8b1b0fefbbd
Steve Wood <steve.wood@manywho.com>
2013-08-10 21:22:08 -0700

    licensing added

--- Draw/Elements/Shared/SharedElementIdAPI.cs
+++ Draw/Elements/Shared/SharedElementIdAPI.cs
@@ -4,6 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Runtime.Serialization;
 
+/*!
+
+Copyright 2013 Manywho, Inc.
+
+Licensed under the Manywho License, Version 1.0 (the "License"); you may not use this
+file except in compliance with the License.
+
+You may obtain a copy of the License at: http://manywho.com/sharedsource
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied. See the License for the specific language governing
+permissions and limitations under the License.
+
+*/
+
 namespace ManyWho.Flow.SDK.Draw.Elements.Shared
 {
     [DataContract(Namespace = "http://www.manywho.com/api")]
