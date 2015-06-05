ae8badbb124b8d2824a488f5572162d5799be2f4
Jonjo McKay <jonjo.mckay@manywho.com>
2015-06-05 20:03:30 +0100

    CORE-755: Added new entity for service invoker requests

---
+++ Service/ServiceRequestAPI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ManyWho.Flow.SDK.Service
+{
+    [DataContract(Namespace = "http://www.manywho.com/api")]
+    public class ServiceRequestAPI
+    {
+        [DataMember]
+        public Guid Id { get; set; }
+        [DataMember]
+        public Guid TenantId { get; set; }
+        [DataMember]
+        public Guid StateId { get; set; }
+        [DataMember]
+        public string Content { get; set; }
+        [DataMember]
+        public string Method { get; set; }
+        [DataMember]
+        public string Uri { get; set; }
+        [DataMember]
+        public DateTime CreatedAt { get; set; }
+    }
+}
