ac901b7fdf902501b9d2e62932fe63750325d152
Jonjo McKay <jonjo.mckay@manywho.com>
2015-06-06 11:28:38 +0100

    CORE-755: Added ServiceFailureAPI and ServiceResponseAPI classes

--- Service/ServiceFailureAPI.cs
+++ Service/ServiceFailureAPI.cs
@@ -1,29 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Runtime.Serialization;
+using ManyWho.Flow.SDK.Errors;
 
 namespace ManyWho.Flow.SDK.Service
 {
     [DataContract(Namespace = "http://www.manywho.com/api")]
-    public class ServiceRequestAPI
+    public class ServiceFailureAPI
     {
         [DataMember]
         public Guid Id { get; set; }
+        [DataMember]
+        public Guid ServiceRequestId { get; set; }
         [DataMember]
         public Guid TenantId { get; set; }
         [DataMember]
         public Guid StateId { get; set; }
         [DataMember]
-        public string Content { get; set; }
-        [DataMember]
-        public string Method { get; set; }
-        [DataMember]
-        public string Uri { get; set; }
+        public ServiceProblem Problem { get; set; }
         [DataMember]
         public DateTime CreatedAt { get; set; }
-        [DataMember]
-        public List<ServiceResponseAPI> Responses { get; set; }
-        [DataMember]
-        public List<ServiceFailureAPI> Failures { get; set; }
     }
 }
