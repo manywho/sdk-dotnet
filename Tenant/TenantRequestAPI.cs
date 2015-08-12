b4b02ecf65a52c1e31e43ce2e2b73e64c5ad9260
Jonjo McKay <jonjo.mckay@manywho.com>
2015-08-12 14:47:14 +0100

    CORE-130: Added subdomain support to the tenant requests and responses

--- Tenant/TenantRequestAPI.cs
+++ Tenant/TenantRequestAPI.cs
@@ -43,5 +43,12 @@ namespace ManyWho.Flow.SDK.Tenant
             get;
             set;
         }
+
+        [DataMember]
+        public string subdomain
+        {
+            get;
+            set;
+        }
     }
 }
