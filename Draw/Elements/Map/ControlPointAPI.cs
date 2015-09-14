9e1a103b2534317c05275f52629e6781b8e238e0
Jonjo McKay <jonjo.mckay@manywho.com>
2015-09-14 16:09:01 +0100

    CORE-1072: Added DataMember annotations to ControlPointAPI

--- Draw/Elements/Map/ControlPointAPI.cs
+++ Draw/Elements/Map/ControlPointAPI.cs
@@ -5,12 +5,14 @@ namespace ManyWho.Flow.SDK.Draw.Elements.Map
     [DataContract(Namespace = "http://www.manywho.com/api")]
     public class ControlPointAPI
     {
+        [DataMember]
         public int x
         {
             get;
             set;
         }
 
+        [DataMember]
         public int y
         {
             get;
