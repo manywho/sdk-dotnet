b4fb8a508c611b9c7681bcb857930463eea8c99d
Jonjo McKay <jonjo.mckay@manywho.com>
2015-09-14 15:55:40 +0100

    CORE-1072: Added missing files

---
+++ Draw/Elements/Map/ControlPointAPI.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace ManyWho.Flow.SDK.Draw.Elements.Map
+{
+    [DataContract(Namespace = "http://www.manywho.com/api")]
+    public class ControlPointAPI
+    {
+        public int x
+        {
+            get;
+            set;
+        }
+
+        public int y
+        {
+            get;
+            set;
+        }
+    }
+}
