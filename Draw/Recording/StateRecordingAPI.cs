52c3dd22a48b68e2401345b8c2725e5ee635a136
Jonjo McKay <jonjo.mckay@manywho.com>
2015-11-30 12:05:35 +0000

    CORE-1156: Added IsPublic to StateRecordingAPI

--- Draw/Recording/StateRecordingAPI.cs
+++ Draw/Recording/StateRecordingAPI.cs
@@ -42,7 +42,7 @@ namespace ManyWho.Flow.SDK.Draw.Recording
         }
 
         [DataMember]
-        public string AuthenticationType
+        public bool IsPublic
         {
             get;
             set;
