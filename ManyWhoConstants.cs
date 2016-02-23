ec47b889e40d4fa50a77ebd45563a90094686be7
Joao Moreira <joao.moreira@manywho.com>
2016-02-23 11:56:12 +0000

    CORE-1619 - Created a Set Equal constant

--- ManyWhoConstants.cs
+++ ManyWhoConstants.cs
@@ -71,6 +71,7 @@ namespace ManyWho.Flow.SDK
         public const String CONTENT_VALUE_IMPLEMENTATION_BASE_COMMAND_FILTER = "FILTER";
         public const String CONTENT_VALUE_IMPLEMENTATION_BASE_COMMAND_GET_LENGTH = "GET_LENGTH"; // gets the length of a list, string, content or password
         public const String CONTENT_VALUE_IMPLEMENTATION_BASE_COMMAND_GET_VALUE = "VALUE_OF"; // gets the content value / objectdata of a Value
+        public const String CONTENT_VALUE_IMPLEMENTATION_BASE_COMMAND_SET_EQUAL = "SET_EQUAL"; // sets the content value / objectdata of a value
 
         // These are the command properties associated with the filter
         public const String CONTENT_VALUE_IMPLEMENTATION_BASE_COMMAND_PROPERTY_FILTER_COLUMN = "COLUMN";
