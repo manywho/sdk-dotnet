3b764e6fe30c59fabfe35bf5165e04680d254f49
matt-manywho <matt.bates@manywho.com>
2016-02-25 08:56:31 +0000

    CORE-1506 - Added OrderBy collection to listfilter

---
+++ Run/Elements/Type/ListFilterOrderByAPI.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+/*!
+
+Copyright 2016 Manywho, Inc.
+
+Licensed under the Manywho License, Version 1.0 (the "License"); you may not use this
+file except in compliance with the License.
+
+You may obtain a copy of the License at: http://manywho.com/sharedsource
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied. See the License for the specific language governing
+permissions and limitations under the License.
+
+*/
+namespace ManyWho.Flow.SDK.Run.Elements.Type
+{
+    [DataContract(Namespace = "http://www.manywho.com/api")]
+    public class ListFilterOrderByAPI
+    {
+        /// <summary>
+        /// The developer name of the column to order by.
+        /// </summary>
+        [DataMember]
+        public String orderByPropertyDeveloperName
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The direction of the ordering.
+        /// </summary>
+        [DataMember]
+        public String orderByDirectionType
+        {
+            get;
+            set;
+        }
+    }
+}
