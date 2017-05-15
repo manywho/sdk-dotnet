594b503095c435c2f677ad0d509a994a7d818f8c
Jonjo McKay <jonjo.mckay@manywho.com>
2017-05-15 15:44:33 +0100

    CORE-3158: Added NotificationAPI object

---
+++ Notification/NotificationAPI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ManyWho.Flow.SDK.Notification
+{
+    public class NotificationAPI
+    {
+        public Guid Id
+        {
+            get;
+            set;
+        }
+
+        public Guid UserId
+        {
+            get;
+            set;
+        }
+
+        public string Content
+        {
+            get;
+            set;
+        }
+
+        public string Subject
+        {
+            get;
+            set;
+        }
+
+        public string Type
+        {
+            get;
+            set;
+        }
+
+        public DateTimeOffset CreatedAt
+        {
+            get;
+            set;
+        }
+    }
+}
