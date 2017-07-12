66a92034afd0f9bf197e74c2ca9d00813f311301
Jonjo McKay <jonjo.mckay@manywho.com>
2017-07-12 10:55:58 +0100

    CORE-3350: Added AssetAPI object

---
+++ Draw/Assets/AssetAPI.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ManyWho.Flow.SDK.Draw.Assets
+{
+    public class AssetAPI
+    {
+        public string Key
+        {
+            get;
+            set;
+        }
+
+        public DateTimeOffset ModifiedAt
+        {
+            get;
+            set;
+        }
+
+        public string PublicUrl
+        {
+            get;
+            set;
+        }
+
+        public long Size
+        {
+            get;
+            set;
+        }
+    }
+}
