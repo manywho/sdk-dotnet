2afd57dd20705743915877557d2d42129e0cd139
Jonjo McKay <jonjo.mckay@manywho.com>
2017-08-15 12:34:06 +0100

    CORE-3350: Added some more asset properties

--- Draw/Assets/AssetAPI.cs
+++ Draw/Assets/AssetAPI.cs
@@ -4,7 +4,19 @@ namespace ManyWho.Flow.SDK.Draw.Assets
 {
     public class AssetAPI
     {
+        public string ContentType
+        {
+            get;
+            set;
+        }
+
         public string Key
+        {
+            get;
+            set;
+        }
+
+        public string Name
         {
             get;
             set;
---
+++ Draw/Assets/AssetMoveRequestAPI.cs
@@ -0,0 +1,17 @@
+﻿namespace ManyWho.Flow.SDK.Draw.Assets
+{
+    public class AssetMoveRequestAPI
+    {
+        public string OldKey
+        {
+            get;
+            set;
+        }
+
+        public string NewKey
+        {
+            get;
+            set;
+        }
+    }
+}
