bda2cea4d7c572bd62be7ca1a84905770056350d
Jonjo McKay <jonjo.mckay@manywho.com>
2017-08-31 12:45:43 +0100

    CORE-3483: Added release cycle settings to tenants

---
+++ Tenant/ReleaseCycle.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace ManyWho.Flow.SDK.Tenant
+{
+    public enum ReleaseCycle
+    {
+        [EnumMember(Value = "rolling")]
+        Rolling = 0,
+        [EnumMember(Value = "monthly")]
+        Monthly = 1
+    }
+}
--- Tenant/TenantSettingsAPI.cs
+++ Tenant/TenantSettingsAPI.cs
@@ -1,30 +1,18 @@
-﻿using System.Runtime.Serialization;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
 namespace ManyWho.Flow.SDK.Tenant
 {
-    [DataContract(Namespace = "http://www.manywho.com/api")]
     public class TenantSettingsAPI
     {
         [JsonConverter(typeof(StringEnumConverter))]
-        [DataMember]
-        public EngineVersion engineVersion
+        public ReleaseCycle ReleaseCycle
         {
             get;
             set;
         }
 
-        public enum EngineVersion
-        {
-            [EnumMember(Value = "current")]
-            Current = 0,
-            [EnumMember(Value = "unstable")]
-            Unstable = 1
-        }
-
-        [DataMember]
-        public bool formatValues
+        public bool FormatValues
         {
             get;
             set;
