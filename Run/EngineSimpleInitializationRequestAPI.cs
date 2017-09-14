cb6cbce8c653da8201c567a5f18a7db10d4f806f
Jonjo McKay <jonjo.mckay@manywho.com>
2017-09-14 14:58:52 +0100

    CORE-3514: Added simple initialization request

---
+++ Run/EngineSimpleInitializationRequestAPI.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ManyWho.Flow.SDK.Run
+{
+    public class EngineSimpleInitializationRequestAPI
+    {
+        public Guid Id
+        {
+            get;
+            set;
+        }
+
+        public Guid VersionId
+        {
+            get;
+            set;
+        }
+
+        public string DeveloperName
+        {
+            get;
+            set;
+        }
+
+        public string Player
+        {
+            get;
+            set;
+        }
+    }
+}
