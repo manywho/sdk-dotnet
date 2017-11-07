b28f38bba0000cbd3cba1da21e44bdcd0010634f
Jonjo McKay <jonjo@jonjomckay.com>
2017-11-06 18:24:01 -0800

    INFO-1043: Moved existing documentation to the Service objects

--- Service/ServiceRequestAPI.cs
+++ Service/ServiceRequestAPI.cs
@@ -8,26 +8,63 @@ namespace ManyWho.Flow.SDK.Service
     [DataContract(Namespace = "http://www.manywho.com/api")]
     public class ServiceRequestAPI
     {
+        /// <summary>
+        /// The unique ID of the Request
+        /// </summary>
         [DataMember]
         public Guid Id { get; set; }
+        
+        /// <summary>
+        /// The unique ID of the tenant the Request belongs to
+        /// </summary>
         [DataMember]
         public Guid TenantId { get; set; }
+        
+        /// <summary>
+        /// The unique ID of the state the Request belongs to
+        /// </summary>
         [DataMember]
         public Guid StateId { get; set; }
+        
+        /// <summary>
+        /// The serialised JSON body of the Request sent to the service
+        /// </summary>
         [DataMember]
         public string Content { get; set; }
+        
+        /// <summary>
+        /// The HTTP method of the call to the service
+        /// </summary>
         [DataMember]
         public string Method { get; set; }
+        
+        /// <summary>
+        /// The URI of the endpoint hit on the service
+        /// </summary>
         [DataMember]
         public string Uri { get; set; }
+        
         [JsonIgnore]
         public string AuthorizationHeader { get; set; }
+        
+        /// <summary>
+        /// The date and time that the request was created and sent at
+        /// </summary>
         [DataMember]
         public DateTime CreatedAt { get; set; }
+        
+        /// <summary>
+        /// A list of responses sent back from the service. This field is only populated when fetching a single Request
+        /// </summary>
         [DataMember]
         public List<ServiceResponseAPI> Responses { get; set; }
+        
+        /// <summary>
+        /// A list of failures sent back from the service. This field is only populated when fetching a single Request
+        /// </summary>
         [DataMember]
         public List<ServiceFailureAPI> Failures { get; set; }
+        
         [DataMember]
         public Dictionary<string, object> Attributes { get; set; }
     }
--- Service/ServiceResponseAPI.cs
+++ Service/ServiceResponseAPI.cs
@@ -6,16 +6,39 @@ namespace ManyWho.Flow.SDK.Service
     [DataContract(Namespace = "http://www.manywho.com/api")]
     public class ServiceResponseAPI
     {
+        /// <summary>
+        /// The unique ID of the Response
+        /// </summary>
         [DataMember]
         public Guid Id { get; set; }
+        
+        /// <summary>
+        /// The unique ID of the Request the Response belongs to
+        /// </summary>
         [DataMember]
         public Guid ServiceRequestId { get; set; }
+        
+        /// <summary>
+        /// The unique ID the tenant the Response belongs to
+        /// </summary>
         [DataMember]
         public Guid TenantId { get; set; }
+        
+        /// <summary>
+        /// The unique ID of the state the Response belongs to
+        /// </summary>
         [DataMember]
         public Guid StateId { get; set; }
+        
+        /// <summary>
+        /// The date and time that the response was created and received at
+        /// </summary>
         [DataMember]
         public DateTime CreatedAt { get; set; }
+        
+        /// <summary>
+        /// The serialised body of the Response sent back from the service
+        /// </summary>
         [DataMember]
         public string Content { get; set; }
     }
