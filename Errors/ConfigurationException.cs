118123956f4febcd6f16685b6c040876e226c9d1
Jonjo McKay <jonjo@jonjomckay.com>
2017-11-25 13:50:55 +0000

    Added element property to ConfigurationException

--- Errors/ConfigurationException.cs
+++ Errors/ConfigurationException.cs
@@ -1,12 +1,24 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 
 namespace ManyWho.Flow.SDK.Errors
 {
     public class ConfigurationException : EngineException
     {
+        public Guid Element
+        {
+            get;
+            set;
+        }
+        
         public ConfigurationException(string message) : base(message, HttpStatusCode.BadRequest)
         {
 
+        }
+
+        public ConfigurationException(string message, Guid element) : base(message)
+        {
+            Element = element;
         }
     }
 }
