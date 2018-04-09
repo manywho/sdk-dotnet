64db0107266d13f93511c8f64af8a2187b2723ee
Jonjo McKay <jonjo.mckay@manywho.com>
2018-04-09 12:53:17 +0100

    CORE-4112: Added StateStatusAPI enum

---
+++ Run/State/StateStatusAPI.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+
+namespace ManyWho.Flow.SDK.Run.State
+{
+    public enum StateStatusAPI
+    {
+        [EnumMember(Value = "")]
+        All = 0,
+        [EnumMember(Value = "done")]
+        Done = 1,
+        [EnumMember(Value = "expired")]
+        Expired = 2,
+        [EnumMember(Value = "in_progress")]
+        InProgress = 3,
+        [EnumMember(Value = "errored")]
+        Errored = 4
+    }
+}
