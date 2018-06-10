fe8835b47a27be69dfa6233f0842724d8d8b4660
Jonjo McKay <jonjo@jonjomckay.com>
2018-06-10 14:29:01 +0100

    CORE-4353: Added a tenant setting for disabling service invoker logging

--- Tenant/TenantSettingsAPI.cs
+++ Tenant/TenantSettingsAPI.cs
@@ -9,6 +9,17 @@ namespace ManyWho.Flow.SDK.Tenant
     public class TenantSettingsAPI
     {
         /// <summary>
+        /// Whether to disable the logging of service invoker requests, reponses and failures. They are helpful when
+        /// debugging, and a requirement for a tenant's flows to be tested using Reflow, but having the platform store
+        /// them might be against a customer's compliance rules.
+        /// </summary>
+        public bool DisableServiceInvokerLogging
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
         /// The version of the platform your tenant’s flows will run using - can be one of `rolling` to always use the 
         /// latest version, or `monthly` for a monthly-released, 1-month delayed version (based on `rolling`)
         /// </summary>
