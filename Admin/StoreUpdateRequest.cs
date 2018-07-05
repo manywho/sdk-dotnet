69c2c658fbfea9513725cf87dda0cbc338982627
Jonjo McKay <jonjo.mckay@manywho.com>
2018-07-05 16:08:28 +0100

    CORE-4452: Added support for updating stores

---
+++ Admin/StoreUpdateRequest.cs
@@ -0,0 +1,23 @@
+﻿namespace ManyWho.Flow.SDK.Admin
+{
+    public class StoreUpdateRequest
+    {
+        /// <summary>
+        /// The (optional) new name of the store
+        /// </summary>
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The (optional) new endpoint of the store, which must support HTTPS.
+        /// </summary>
+        public string Endpoint
+        {
+            get;
+            set;
+        }
+    }
+}
