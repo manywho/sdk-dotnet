6f62d76073f73ad19511fe01ded6234263725cc1
Jonjo McKay <jonjo.mckay@manywho.com>
2018-07-12 11:07:07 +0100

    CORE-4476: Added support for saving states to the platform and externally simultaneously

--- Tenant/ExternalStorageSettingsAPI.cs
+++ Tenant/ExternalStorageSettingsAPI.cs
@@ -5,6 +5,16 @@ namespace ManyWho.Flow.SDK.Tenant
     public class ExternalStorageSettingsAPI
     {
         /// <summary>
+        /// Whether to enable saving data to both the platform and the store simultaneously. This functionality is
+        /// intended to ease the process of a full migration to external-only storage.
+        /// </summary>
+        public bool EnableReplication
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
         /// Whether to store states using the External Storage API or not
         /// </summary>
         public bool EnableStates
