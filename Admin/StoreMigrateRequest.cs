c08cd1ebe621bde1d2921c2939f313e8a8afedef
Jacek Wojciechowski <jacek_wojciechowski@dell.com>
2018-07-25 16:56:18 +0100

    CORE-4487: migrate states to external state storage - initial implementation

---
+++ Admin/StoreMigrateRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace ManyWho.Flow.SDK.Admin
+{
+    public class StoreMigrateRequest
+    {
+        /// <summary>
+        /// A size of batch that will be used to save states during migration
+        /// </summary>
+        public int? BatchSize
+        {
+            get;
+            set;
+        }
+    }
+}
