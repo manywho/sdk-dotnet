b144c39e6af91f2e076bea36c6904650d75dc42b
Jonjo McKay <jonjo.mckay@manywho.com>
2018-10-02 14:39:45 +0100

    CORE-4197: Added missing file

---
+++ Draw/Elements/Type/ListFilterPropertyConfigAPI.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace ManyWho.Flow.SDK.Draw.Elements.Type
+{
+    public class ListFilterPropertyConfigAPI
+    {
+        /// <summary>
+        /// The ID of the property on the type element
+        /// </summary>
+        public Guid TypeElementPropertyId
+        {
+            get;
+            set;
+        }
+    }
+}
