fd4609c3df5478f7178465f29d22faf6832f5dae
Jacek Wojciechowski <jacek_wojciechowski@dell.com>
2018-11-07 15:49:29 +0000

    CORE-4886: Saving effective_tenant_id and effective_user_id to Statesjson table for System Flows

--- Run/State/StateAPI.cs
+++ Run/State/StateAPI.cs
@@ -202,5 +202,19 @@ namespace ManyWho.Flow.SDK.Run.State
             get;
             set;
         }
+        
+        [DataMember]
+        public Guid? effectiveTenantId
+        {
+            get;
+            set;
+        }
+        
+        [DataMember]
+        public Guid? effectiveUserId
+        {
+            get;
+            set;
+        }
     }
 }
