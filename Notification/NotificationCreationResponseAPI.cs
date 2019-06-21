88eb4f93d3bb6b291832a7667fbe9ebf2d7ee8e7
Jonjo McKay <jonjo@jonjomckay.com>
2019-06-21 15:28:30 +0100

    CORE-5325: Added NotificationCreationResponseAPI type

---
+++ Notification/NotificationCreationResponseAPI.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ManyWho.Flow.SDK.Notification
+{
+    public class NotificationCreationResponseAPI
+    {
+        public Guid? Id
+        {
+            get;
+            set;
+        }
+    }
+}
