2aa135e3e8a4059f9e9bba8c8840b1e30ce7ec3f
Jonjo McKay <jonjo@jonjomckay.com>
2019-07-20 14:42:21 +0100

    CORE-5147: Added SAML settings to the tenant object

---
+++ Security/SamlSettings.cs
@@ -0,0 +1,23 @@
+namespace ManyWho.Flow.SDK.Security
+{
+    public class SamlSettings
+    {
+        /// <summary>
+        /// The public certificate used for validating the signature of any SAML objects.
+        /// </summary>
+        public string Certificate
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The URI to redirect users to, in order for them to authenticate in the IdP.
+        /// </summary>
+        public string RedirectUri
+        {
+            get;
+            set;
+        }
+    }
+}
