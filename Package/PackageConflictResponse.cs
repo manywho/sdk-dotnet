626ff5d451f901f3e6283094061d06234a9936c9
Jonjo McKay <jonjo@jonjomckay.com>
2019-09-25 13:26:23 +0100

    CORE-5422: Added some documentation

--- Package/PackageConflictResponse.cs
+++ Package/PackageConflictResponse.cs
@@ -1,7 +1,13 @@
 namespace ManyWho.Flow.SDK.Package
 {
+    /// <summary>
+    /// A response that details the conflicts that arose from a packaging operation.
+    /// </summary>
     public class PackageConflictResponse
     {
+        /// <summary>
+        /// Information about any existing elements that were detected
+        /// </summary>
         public PackageElements ExistingElements
         {
             get;
--- Package/PackageElement.cs
+++ Package/PackageElement.cs
@@ -2,14 +2,23 @@ using System;
 
 namespace ManyWho.Flow.SDK.Package
 {
+    /// <summary>
+    /// A summary of an element from a packaging operation.
+    /// </summary>
     public class PackageElement
     {
+        /// <summary>
+        /// The ID of the element
+        /// </summary>
         public Guid Id
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// The name of the element
+        /// </summary>
         public string DeveloperName
         {
             get;
