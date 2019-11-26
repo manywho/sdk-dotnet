4ddecf8c109d013c75cb7aafb874ae136c953f4c
Jacek Wojciechowski <jacek_wojciechowski@dell.com>
2019-11-26 17:28:49 +0000

    CORE-5389: fields doc

--- Run/Flow/FlowStackFrameAPI.cs
+++ Run/Flow/FlowStackFrameAPI.cs
@@ -10,6 +10,9 @@ namespace ManyWho.Flow.SDK.Run.Flow
     public class FlowStackFrameAPI
     {
 
+        /// <summary>
+        /// The id of the flow associated with this stack frame
+        /// </summary>
         [DataMember]
         public FlowIdAPI FlowId
         {
@@ -17,6 +20,10 @@ namespace ManyWho.Flow.SDK.Run.Flow
             set;
         }
         
+        /// <summary>
+        /// If this frame is the current frame, this property contains the id of the current map element.
+        /// If this frame refers to one of the parent frames, this property contains the id of a Subflow map element that was the entry point to the next frame. 
+        /// </summary>
         [DataMember]
         public Guid? MapElementId
         {
@@ -24,6 +31,10 @@ namespace ManyWho.Flow.SDK.Run.Flow
             set;
         }
 
+        /// <summary>
+        /// If this frame is the current frame, this property contains the developer name of the current map element.
+        /// If this frame refers to one of the parent frames, this property contains the id of a Subflow map element that was the entry point to the next frame. 
+        /// </summary>
         [DataMember]
         public string MapElementDeveloperName
         {
@@ -31,6 +42,10 @@ namespace ManyWho.Flow.SDK.Run.Flow
             set;
         }
 
+        /// <summary>
+        /// This is our reference running user.  By default, the running user is the person who initializes the flow.  From then on,
+        /// the running user can be re-assigned as needed - but it is always a single user identity that is tagged as the running user.
+        /// </summary>
         [DataMember]
         public string RunningUserId
         {
@@ -38,6 +53,9 @@ namespace ManyWho.Flow.SDK.Run.Flow
             set;
         }
         
+        /// <summary>
+        /// The state entry representing changes to the state that have been made since the last commit
+        /// </summary>
         [DataMember]
         public StateEntryAPI PrecommitStateEntry
         {
@@ -45,6 +63,9 @@ namespace ManyWho.Flow.SDK.Run.Flow
             set;
         }
         
+        /// <summary>
+        /// The list of value elements that are associated with this frame
+        /// </summary>
         [DataMember]
         public List<StateValueAPI> Values
         {
