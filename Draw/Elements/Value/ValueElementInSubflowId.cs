022ca6d3a9f483c4816e49fde3d1ad3fa1b6eac7
Jacek Wojciechowski <jacek_wojciechowski@dell.com>
2019-11-29 14:34:32 +0000

    CORE-5511: changing the property class for valueElementInSubflowId

---
+++ Draw/Elements/Value/ValueElementInSubflowId.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace ManyWho.Flow.SDK.Draw.Elements.Value
+{
+    [DataContract(Namespace = "http://www.manywho.com/api")]
+    public class ValueElementInSubflowIdAPI
+    {
+        public ValueElementInSubflowIdAPI()
+        {
+
+        }
+
+        public ValueElementInSubflowIdAPI(Guid id)
+        {
+            this.id = id.ToString();
+        }
+        
+        /// <summary>
+        /// The unique identifier for the Value to be referenced with this identifier.
+        /// </summary>
+        [DataMember]
+        public string id
+        {
+            get;
+            set;
+        }
+    }
+}
