70e4fa69e6d9673b306c9e4a1bf1cd62efac1c5f
Jonjo McKay <jonjo@jonjomckay.com>
2019-12-20 15:13:52 +0000

    CORE-5556: Added API object for inviting tenants to organizations

---
+++ Admin/Organization/OrganizationInviteTenantRequest.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ManyWho.Flow.SDK.Admin.Organization
+{
+    public class OrganizationInviteTenantRequest
+    {
+        public string DeveloperName
+        {
+            get;
+            set;
+        }
+
+        public Guid Id
+        {
+            get;
+            set;
+        }
+    }
+}
