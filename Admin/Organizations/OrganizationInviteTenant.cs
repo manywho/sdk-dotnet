dd2d25688d749658ca91c5eeaf4d875fe3b1f2bc
Jonjo McKay <jonjo@jonjomckay.com>
2020-03-05 15:57:33 +0000

    CORE-5556: Added documentation

--- Admin/Organizations/OrganizationInviteTenant.cs
+++ Admin/Organizations/OrganizationInviteTenant.cs
@@ -6,42 +6,63 @@ namespace ManyWho.Flow.SDK.Admin.Organizations
 {
     public class OrganizationInviteTenant
     {
+        /// <summary>
+        /// The organization that sent the invitation
+        /// </summary>
         public OrganizationMinimal Organization
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// The tenant that is invited to join the organization
+        /// </summary>
         public TenantMinimalAPI Tenant
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// The user who either accepted or rejected this invitation
+        /// </summary>
         public BuilderWhoAPI CompletedBy
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// When the invitation was either accepted or rejected
+        /// </summary>
         public DateTimeOffset? CompletedAt
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// The user who sent the invitation
+        /// </summary>
         public BuilderWhoAPI InvitedBy
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// When the invitation was sent
+        /// </summary>
         public DateTimeOffset InvitedAt
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// The current status of the invitation. Can be one of "invited", "accepted" or "rejected".
+        /// </summary>
         public string Status
         {
             get;
--- Admin/Organizations/OrganizationMinimal.cs
+++ Admin/Organizations/OrganizationMinimal.cs
@@ -4,12 +4,18 @@ namespace ManyWho.Flow.SDK.Admin.Organizations
 {
     public class OrganizationMinimal
     {
+        /// <summary>
+        /// The ID of the organization
+        /// </summary>
         public Guid Id
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// The friendly name of the organization
+        /// </summary>
         public string Name
         {
             get;
