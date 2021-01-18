24f7857de32ca1f0c1eb9bfee65e40e0ee10605b
matt-manywho <Matthew_Bates@Dell.com>
2021-01-18 14:21:09 +0000

    FLOW-2874 - Removed reflow specific bits

--- Fuid.cs
+++ Fuid.cs
@@ -3,18 +3,10 @@
 namespace ManyWho.Flow.SDK
 {
     public static class Fuid
-    {        
-        public static bool ReflowEnabled { private get; set; }
-
+    {  
         public static Guid NewGuid()
         {
-            Guid newGuid = Guid.NewGuid();
-
-            if (ReflowEnabled)
-            {
-                newGuid = Guid.Parse( newGuid.ToString("N").Substring(0, 26) + "ffffff" );
-            }
-            
+            Guid newGuid = Guid.NewGuid();            
             return newGuid;
         }
     }
